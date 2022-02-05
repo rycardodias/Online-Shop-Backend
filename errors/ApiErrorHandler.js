ed5ddd15919f26e47f126ac1897d11d8d672bb21
rycardodias <rycardo.dias@hotmail.com>
2022-02-05 00:43:49 +0000

    errorSequelizeDone

--- errors/ApiErrorHandler.js
+++ errors/ApiErrorHandler.js
@@ -2,14 +2,16 @@ const ApiError = require("./ApiError")
 
 function ApiErrorHandler(err, req, res, next) {
     if (err instanceof ApiError) {
-        let errorsArray = []
+
         if (err.objectMessage) {
+            let errorsArray = []
             for (const element of err.objectMessage) {
                 errorsArray.push(req.t(element.type, { field: element.field }))
                 // console.log(req.t(element.type, { field: element.field }));
             }
-            return res.status(err.code).json({ error: errorsArray }) 
+            return res.status(err.code).json({ error: errorsArray })
         }
+
         return res.status(err.code).json({ error: req.t(err.message) })
     }
     return res.status(500).json({ error: req.t('something_went_wrong') })
